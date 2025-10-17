5c1acb8bafca6e93a8f3c8dd917faf5168b45aa3
TomasLonnblad <tomaslonnblad@gmail.com>
2025-10-17 05:51:39 +0200

    x

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
 
+/**
+ * @type {import('next').NextConfig}
+ */
+const isProd = process.env.NODE_ENV = 'production';
+const nextConfig = {
+  basePath: isProd ? '/nextjs-blog-deployment' : '',
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+ },
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
 module.exports = nextConfig
